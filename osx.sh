cadc8e168f51deee68a2599d81d07462b7123959
Christian Claus <ch.claus@me.com>
2017-09-30 14:15:57 +0200

    remove boot2docker

--- osx.sh
+++ osx.sh
@@ -10,10 +10,12 @@ fi
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 brew update && brew upgrade
-brew install nvm
+#brew install nvm
 brew install git
 brew install wget
 brew install golang
 brew install maven
 brew install ghostscript
 brew install ansible 
+
+#nvm install 4
